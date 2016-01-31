5023fe936f6604541302de0285ca1b63c6bfdf97
Sophia Richter <sophiarichter12@gmail.com>
2016-01-31 18:27:57 -0500

    Initial Commit

---
+++ Homework 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework 1
+//
+//  Created by Sophia Richter on 1/31/16.
+//  Copyright © 2016 Sophia Richter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
